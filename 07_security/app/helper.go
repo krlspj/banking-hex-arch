17fda26227a7e741d0c7e4d3412e0deaaabed6ae
Krls08 <49446117+krls08@users.noreply.github.com>
2023-01-08 03:28:11 +0100

    Create helper.go

---
+++ 07_security/app/helper.go
@@ -0,0 +1,9 @@
+package app
+
+func writeResponse(w http.ResponseWriter, code int, data any) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
+	}
+}
